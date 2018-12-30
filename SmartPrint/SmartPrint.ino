9e6ffe8d9a6a10d77f488d9a3d5aeba7b889e5c1
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2018-12-30 19:58:30 -0200

    Primeiro commit

---
+++ SmartPrint/SmartPrint.ino
@@ -0,0 +1,18 @@
+/*
+ * 
+ * Autor......................: Jonas Henrique Nascimento
+ * Nickname...................: W8jonas
+ * Data de inicio.............: 30/12/2018
+ * Ultima alteração do sketch.: 30/12/2018
+ * 
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
